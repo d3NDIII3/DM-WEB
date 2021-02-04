2a93a2b031ccf0211f050e859869029c82574813
nightc0der <76710894+nightc0der@users.noreply.github.com>
2021-02-04 14:33:24 +0200

    first  commit

---
+++ client/src/components/header.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./header.css";
+
+
+class Header extends Component {
+  
+  state = {};
+  render() {
+   
+    return (
+      <div className="header">
+      
+
+      </div>
+    );
+  }
+}
+
+export default Header;
